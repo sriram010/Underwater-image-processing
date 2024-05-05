699ca9531a9b6b16dbcc77e999744fbc04702218
Sudarsan P <apsudarsan2002@gmail.com>
2024-05-06 00:20:49 +0530

    Added functionality to save processed image after each step

--- Backend/ImageProcessing/main.py
+++ Backend/ImageProcessing/main.py
@@ -14,10 +14,26 @@ alpha = 1
 window_size = 5
 
 # Calling all the image processing functions
+print("Starting to process the input image")
 compensated_image = RedChannelCompensation(image, alpha, window_size)
+output_path = "E:/mini_project/Underwater-image-processing/Backend/ImageProcessing/OutputImages/red_channel_compensated_image.png"
+cv2.imwrite(output_path, compensated_image)
+print(f"Red Channel Compensated image saved as {output_path}.")
+
 white_balanced_image = WhiteBalance(compensated_image)
+output_path = "E:/mini_project/Underwater-image-processing/Backend/ImageProcessing/OutputImages/white_balanced_image.png"
+cv2.imwrite(output_path, white_balanced_image)
+print(f"White Balanced image saved as {output_path}.")
+
 retinex_image = RetinexWithDensePixels(white_balanced_image)
+output_path = "E:/mini_project/Underwater-image-processing/Backend/ImageProcessing/OutputImages/retinex_image.png"
+cv2.imwrite(output_path, retinex_image)
+print(f"Retinex image saved as {output_path}.")
+
 final_image = AdaptiveHistogramTransform(retinex_image)
+output_path = "E:/mini_project/Underwater-image-processing/Backend/ImageProcessing/OutputImages/adaptive_histogram_image.png"
+cv2.imwrite(output_path, compensated_image)
+print(f"Adaptive Histogram image saved as {output_path}.")
 
 # Saving the output image path
 output_path = "E:/mini_project/Underwater-image-processing/Frontend/public/processed_image.png"
